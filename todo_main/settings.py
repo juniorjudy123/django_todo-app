aa6ab97af116eff3e4245b942afc8359f5a20998
benoy_Judy <benoyjudy@gmail.com>
2025-11-16 11:28:09 +0530

    app creations,admin panel changes,

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'todo_main',
+    'todos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
