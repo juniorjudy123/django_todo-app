bb6d9948593dcd37e5056bc154b813eaaba8e44d
benoy_Judy <benoyjudy@gmail.com>
2025-11-17 13:55:06 +0530

    completedtasks functionality done

--- todo_main/views.py
+++ todo_main/views.py
@@ -6,7 +6,12 @@ def home (request):
     tasks = Task.objects.filter(is_completed=False).order_by(
       'updated_at'  
     )
+    completed_tasks= Task.objects.filter(is_completed= True)
+
+    
     context={
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
+
     }
     return render (request,'home.html',context)
