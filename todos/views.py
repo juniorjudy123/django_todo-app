de5f1d84f8ec28bd71f3d2be68eedd9e34ac9a12
benoy_Judy <benoyjudy@gmail.com>
2025-11-19 11:29:35 +0530

    edit task and delete task complete

--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,50 @@
 
 from django.http import HttpResponse
 from .models import Task
-from django.shortcuts import redirect,render
+from django.shortcuts import redirect,render,get_object_or_404
 # Create your views here.
 def  addTask(request):
     task= request.POST['task']
     Task.objects.create(task=task)
     
+    return redirect('home')
+
+def  mark_as_done(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = True
+    task.save()
+
+    return redirect('home')
+
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = False
+    task.save()
+
+    return redirect('home')
+
+
+def edit_task(request,pk):
+   get_task = get_object_or_404(Task,pk=pk)
+   print( get_task)
+
+   if request.method == 'POST':
+      new_task = request.POST['task']
+      get_task.task = new_task
+      get_task.save()
+
+      return redirect('home')
+   
+   else:
+       context={
+           'get_task':get_task,
+       }
+       return render(request, 'edit_task.html',context)
+   
+
+
+def delete_task(request,pk):
+    task_to_be_deleted=get_object_or_404(Task,pk=pk)
+    task_to_be_deleted.delete()
     return redirect('home')
